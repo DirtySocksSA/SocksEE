da66cdc56937d0a2a97b9028e26f5a7e8513704c
skoutsoutos <Stefanos.KOUTSOUTOS@intrasoft-intl.com>
2017-09-28 18:11:53 +0300

    a matter of initialization

---
+++ Pair of Socks 9.java
@@ -0,0 +1,11 @@
+// initialization matters ...
+
+String s = StringUtils.EMPTY;
+
+if(args.length != 1){
+    System.out.println("........");
+    System.exit(0);
+}
+
+int exitCode = 1;
+s = args[0];
