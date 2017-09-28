10c808bb2ddee7d74784027463379bdc464f17f8
skoutsoutos <Stefanos.KOUTSOUTOS@intrasoft-intl.com>
2017-09-28 17:53:05 +0300

    no comment

---
+++ Pair of Socks 7.java
@@ -0,0 +1,6 @@
+
+if (isLevel2) {
+    finalLevel = 2;
+} else if (isLevel3) {
+    finalLevel = 3;
+}
---
+++ Pair of Socks 8.java
@@ -0,0 +1,2 @@
+LogMF.error(log, "{0} --> Error validating password: {1}", new String[] { method, e.getMessage() });
+LogMF.debug(log, e, null, null);
