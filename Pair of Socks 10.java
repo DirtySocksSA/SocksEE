c8323212eedc48ed21dedc64754150aba3cb24af
skoutsoutos <Stefanos.KOUTSOUTOS@intrasoft-intl.com>
2017-09-28 18:20:15 +0300

    at least they got the comments right :)

---
+++ Pair of Socks 10.java
@@ -0,0 +1,5 @@
+// besides this one, the following comments DO appear in the code
+
+
+// I will work with a list of Integers, cause with that sorting works.
+List takeOverContractsAsIntegers = new ArrayList();
